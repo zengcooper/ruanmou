054af6c163235e9b07042874e5808963f58f8c52
曾加维 <zengjw006045@rich-healthcare.com>
2018-11-27 11:03:51 +0800

    netcore 2.1 json 文本配置 依赖注入 控制反转

---
+++ CoreWeb/Controllers/ContentController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CoreWeb.Models;
+using Microsoft.Extensions.Options;
+
+namespace CoreWeb.Controllers
+{
+    public class ContentController : Controller
+    {
+        private readonly Content contents;
+        /// <summary>
+        /// 首页显示
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            //var contents = new List<Content>();
+            //for (int i = 1; i < 11; i++)
+            //{
+            //    contents.Add(new Content { Id = i, title = $"{i}的标题", content = $"{i}的内容", status = 1, add_time = DateTime.Now.AddDays(-i) });
+            //}
+            // return View(new ContentViewModel { Contents = contents });
+            return View(new ContentViewModel { Contents = new List<Content> { contents } });
+        }
+        public ContentController(IOptions<Content> option)
+        {
+            contents = option.Value;
+        }
+    }
+}
---
+++ CoreWeb/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace CoreWeb
+{
+    public class Program
+    {
+        /// Main方法，程序的入口方法
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args)//调用下面的方法，返回一个IWebHostBuilder对象
+                .Build()//用上面返回的IWebHostBuilder对象创建一个IWebHost
+                .Run();//运行上面创建的IWebHost对象从而运行我们的Web应用程序换句话说就是启动一个一直运行监听http请求的任务
+        }
+
+        //public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        //    WebHost.CreateDefaultBuilder(args)//使用默认的配置信息来初始化一个新的IWebHostBuilder实例
+        //        .UseStartup<Startup>();// 为Web Host指定了Startup类
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+           WebHost.CreateDefaultBuilder(args)//使用默认的配置信息来初始化一个新的IWebHostBuilder实例
+           .ConfigureAppConfiguration((hostingContext, config) =>
+           {
+               var env = hostingContext.HostingEnvironment;
+
+               config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                     .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                     .AddJsonFile("Content.json", optional: false, reloadOnChange: false)
+                     .AddEnvironmentVariables();
+
+           })
+           .UseStartup<Startup>();// 为Web Host指定了Startup类
+    }
+}
